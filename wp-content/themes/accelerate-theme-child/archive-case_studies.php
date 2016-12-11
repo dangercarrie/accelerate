7b897a490ef54c664cc71a8d598d0be360955613
Danger Carrie <dangercarrie@gmail.com>
2016-12-11 13:45:40 -0600

    archive and contact page

--- wp-content/themes/accelerate-theme-child/archive-case_studies.php
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -16,12 +16,38 @@ get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
+			
+			<?php query_posts('post_type=case_studies&order=ASC'); ?>
+			<?php while ( have_posts() ) : the_post();
+
+				$image_1 = get_field('image_1'); 
+				$size = "large";
+				$services = get_field('services');  ?>
+				
+				<article class='case-study'>
+			<aside class ='case-study-sidebar archive'>
+				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+				<h6><?php echo $services; ?></h6>
+				
+				<?php the_excerpt(); ?>
+				<p><strong><a href="<?php the_permalink(); ?>">View Project > </a></strong></p>
+			</aside>
+
+			<div class="case-study-images archive">
+				<a href="<?php the_permalink(); ?>">
+				<?php if($image_1) { 
+					echo wp_get_attachment_image( $image_1, $size );	
+			 		} ?>
+				</a>
+
+				</div>
+			</article>	
+
+
 			<?php endwhile; // end of the loop. ?>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
