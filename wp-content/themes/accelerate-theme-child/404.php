11865f45cba0ca9b93a00b9537737e829b75b098
Danger Carrie <dangercarrie@gmail.com>
2016-12-12 08:51:35 -0600

    404 and custom about

--- wp-content/themes/accelerate-theme-child/404.php
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -17,12 +17,16 @@ get_header(); ?>
 	<div id="primary" class="site-content">
 		<div id="content" class= "contact" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
+			<p>OOOPS!</p>
 			<h2><?php the_title(); ?></h2>
-				<?php the_content(); ?>
+			<?php the_content(); ?>
+			
+
+				
 			<?php endwhile; // end of the loop. ?>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
